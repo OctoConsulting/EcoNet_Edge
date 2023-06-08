7be430307d2e6997a53dd9f3f2fdbde2a4d2caf1
Brissot <sidsu7@gmail.com>
2023-06-08 16:51:08 -0400

    More Query Stuff

---
+++ Edge_DB/put_queries.py
@@ -0,0 +1,58 @@
+# written for Team Edge by Sid Su (c) Octo Consulting LLC 2023
+# Functions to put data into the database
+
+import psycopg
+
+db_info= "host= 172.18.0.3 \
+    dbname=echonet \
+    user= postgres \
+    password= changemeoctobby" # TODO: make not hardcoded, pw security
+
+# function to add a new shot
+def put_shot_raw(shot_time: str, preprocessed_audio_hash: str):
+    connection= psycopg.connect(db_info)
+    current= connection.cursor()
+
+    sql_code= f'''
+    INSERT INTO shots (shot_time, preprocessed_audio_hash)
+    VALUES (\'{shot_time}\', \'{preprocessed_audio_hash}\');
+    '''
+
+    print(sql_code)
+    current.execute(sql_code)
+    connection.commit()
+
+    connection.close()
+    current.close()
+
+# function to add processing data to a shot row
+# def put_shot_processed
+def put_shot_processed(jason):
+        connection= psycopg.connect(db_info)
+    current= connection.cursor()
+
+    sql_code= f'''
+    INSERT INTO shots (shot_time, preprocessed_audio_hash)
+    VALUES (\'{shot_time}\', \'{preprocessed_audio_hash}\');
+    '''
+
+    print(sql_code)
+    current.execute(sql_code)
+    connection.commit()
+
+    connection.close()
+    current.close()
+
+
+'''
+    shot_time timestamp
+    process_time timestamp,
+    event_id int,
+    preprocessed_audio_hash VARCHAR(40), -- currently using SHA1HASH
+    postprocessed_audio_hash VARCHAR(40),
+    relative_coords spherical, -- theta, phi, r
+    absolute_coords gps, -- lat, long
+    gun_type gun, -- gun most likely
+    gun_data jsonb -- json of gun data w/ confidence
+);
+'''
---
+++ Edge_DB/put_test.py
@@ -0,0 +1,3 @@
+import put_queries
+
+put_queries.put_shot("2023-04-20 10:12:32 +05:00", "7820b2a002681a7e887bdd8fa73cbc0292ffec1c")
