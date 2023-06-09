869b630b63f1f092fa8ba4a874f865accd344dc1
hershelr3 <rinker.hershel@gmail.com>
2023-06-09 11:35:09 -0400

    detctor update can send file but not with api

--- Edge_Detector/app.py
+++ Edge_Detector/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sock import Sock
+from flask import Flask, send_file
 import subprocess
-
+import os
 
 app = Flask(__name__)
 sock = Sock(app)
@@ -17,22 +18,32 @@ def detect_shot():
    #     return 'No selected file'
     # send file to shot detector
 
-    result = subprocess.run(['python', '-c', 'import random; print(random.randint(0, 1))'], stdout=subprocess.PIPE)
-    output = result.stdout.decode('utf-8').strip()
-    random_number = int(output)
+    # result = subprocess.run(['python', '-c', 'import random; print(random.randint(0, 1))'], stdout=subprocess.PIPE)
+    # output = result.stdout.decode('utf-8').strip()
+    # random_number = int(output)
+    subprocess_cmd = ['python', 'detector_out.py']
+    subprocess_output = subprocess.run(subprocess_cmd, capture_output=True, text=True)
+    if subprocess_output.returncode != 0:
+            return jsonify({'error': 'Subprocess failed'}), 500
+   
+    
+    output_filename = subprocess_output.stdout.strip()
+    directory_path , filename = os.path.split(output_filename)
+    process_detect_path = os.path.join(directory_path, filename)
 
-    # result = subprocess.run(['python', 'detector_out.py'], stdout=subprocess.PIPE)
-    # temp = result.stdout.decode('utf-8').strip()
-    # print(temp)
-
+    if not os.path.exists(process_detect_path):
+         return jsonify({'error': 'Processed file not found'}), 500
+    
+    
     # get return and return
-    shot = False
-    if random_number == 1:
-        shot = True
+    # shot = False
+    # if random_number == 1:
+    #     shot = True
 
-    resp = {
-        'shot':shot
-    }
+    # resp = {S
+    #     'shot':shot
+    # }
 
-    return jsonify(resp)
+    # return jsonify(resp)
+    return send_file(process_detect_path, as_attachment=True)
         
--- Edge_Detector/detector_out.py
+++ Edge_Detector/detector_out.py
@@ -1,20 +1,54 @@
 from flask import Flask, request, jsonify
 from flask_sock import Sock
 import random
-
+import sys
+import shutil
+import os
 
 app = Flask(__name__)
 sock = Sock(app)
 
-
+#write to standard out and send to standard in "stdout" and "stdin"
+#return a dictionary with the contents of the file as a byte array and a boolean
 def main():
-    #with open("testD.txt", "w") as f:
-    #    f.write('Hello docker!')
-     #  f.close()
-    #...
-    print(random.randint(0, 1))
+    try:
+        # Do any processing on the audio data here
+        # For demonstration purposes, this example simply copies the input file to a new output file
+        global output_filename
+        global final_output
+        randomN = random.randint(0, 1)
+        directory_path = ""
+        
+        filename = "output.txt"
+        with open(filename, "w") as f:
+            if randomN == 1:
+                f.write(str("Shot Detected!"))
+            else:
+                f.write(str("No Shot Detected!"))
 
+        with open(filename, 'r')as f:
+            contents = f.read()
 
-if __name__ == "__main__":
+        output_filename = filename.rsplit('.', 1)[0]  # Remove the extension
+        output_path = output_filename + '_processed.txt'
+        final_output = directory_path + output_path
+        shutil.copyfile(filename, final_output)
+    except Exception as e:
+        sys.stderr.write(str(e))
+        sys.exit(1)
+
+if __name__ == '__main__':
+    # if len(sys.argv) != 2:
+    #     sys.stderr.write('Usage: python audioPlayer.py <input_wav_file>\n')
+    #     sys.exit(1)
+    #takes in argument from client.py
+
+    #shotD = sys.argv[1]
+
+    #sends the argument to the function
+
+    #process_audio(shotD)
+    #sends the argument back into stdout to be read by mian
     main()
+    sys.stdout.write(final_output)
 
