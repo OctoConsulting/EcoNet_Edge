5aec4e2bb3976f07b468d6a650c7282f44e21bb7
Brissot <sidsu7@gmail.com>
2023-06-14 16:48:13 -0400

    Final Commit of the Day :)

--- Edge_DB/test_init.py
+++ Edge_DB/test_init.py
@@ -11,7 +11,7 @@ db_info= "host= db \
     password= changemeoctobby" # TODO: make not hardcoded
 
 with psycopg.connect(db_info) as connection, connection.cursor(row_factory= dict_row) as current:
-  copy_sql = """
+  copy_sql= """
     copy shots(shot_time, process_time, event_id, preprocessed_audio_hash, postprocessed_audio_hash, distance, microphone_angle, shooter_angle, latitude, longitude, gun_type)
     from stdin with
     csv
@@ -19,9 +19,22 @@ with psycopg.connect(db_info) as connection, connection.cursor(row_factory= dict
     delimiter as ','
   """
 
-  from_csv = './test_shots.csv'
+  from_csv= './test_shots.csv'
   with open(from_csv, 'r') as f:
-    data = f.read()
+    data= f.read()
     with current.copy(copy_sql) as copy:
-      copy.write(data)  
+      copy.write(data)
 
+  copy_sql= """
+    copy shot_stats(id, distance_predict, distance_upper, distance_lower, microphone_angle_predict, microphone_angle_upper, microphone_angle_lower, shooter_angle_predict, shooter_angle_upper, shooter_angle_lower, latitude_predict, latitude_upper, latitude_lower, longitude_predict, longitude_upper, longitude_lower, rifle_predict, rifle_upper, rifle_lower, pistol_predict, pistol_upper, pistol_lower)
+    from stdin with
+    csv
+    header
+    delimiter as ','
+  """
+
+  from_csv= './test_shot_stats.csv'
+  with open(from_csv, 'r') as f:
+    data= f.read()
+    with current.copy(copy_sql) as copy:
+      copy.write(data)
