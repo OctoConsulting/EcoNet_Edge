f6a2b94857ad9430c195903972631dc16b9ae2ea
Brissot <sidsu7@gmail.com>
2023-06-20 14:19:38 -0400

    Container & Organization
    Updated the Parrot dev container to reflect our current libraries and needs, and organized the code a bit

---
+++ Edge_Parrot/main_drone.py
@@ -0,0 +1,70 @@
+from flask import Flask, request
+import parrot_methods
+
+ 
+
+app = Flask(__name__)
+
+ 
+
+# Dictionary to store information about active drones
+active_drones = {}
+
+ 
+
+
+@app.route('/drones/<drone_id>/command', methods=['POST'])
+def send_command(drone_id):
+    command = request.json['command']
+    if drone_id in active_drones:
+        # Send the command to the specified drone
+        # Your logic to send the command to the drone goes here
+        return f"Command '{command}' sent to Drone {drone_id}"
+    else:
+        return f"Drone {drone_id} not found"
+
+ 
+
+
+@app.route('/drones/<drone_id>/status')
+def get_status(drone_id):
+    if drone_id in active_drones:
+        # Retrieve and return the status of the specified drone
+        # Your logic to retrieve the drone status goes here
+        return f"Status of Drone {drone_id}"
+    else:
+        return f"Drone {drone_id} not found"
+
+ 
+
+
+@app.route('/drones')
+def get_active_drones():
+    return "Active Drones: " + ', '.join(active_drones.keys())
+
+ 
+
+
+@app.route('/drones/<drone_id>', methods=['POST'])
+def add_drone(drone_id):
+    if drone_id not in active_drones:
+        # Add the new drone to the active drones dictionary
+        active_drones[drone_id] = {}
+        return f"Drone {drone_id} added successfully"
+    else:
+        return f"Drone {drone_id} already exists"
+
+ 
+
+
+@app.route('/drones/<drone_id>', methods=['DELETE'])
+def remove_drone(drone_id):
+    if drone_id in active_drones:
+        # Remove the specified drone from the active drones dictionary
+        del active_drones[drone_id]
+        return f"Drone {drone_id} removed successfully"
+    else:
+        return f"Drone {drone_id} not found"
+
+if __name__ == '__main__':
+    app.run()
