11b8aa4729d6ab28910458e658c9ad36bb259ebe
hershelr3 <rinker.hershel@gmail.com>
2023-06-21 09:42:09 -0400

    test lat and long for parrot

--- Edge_Parrot/test_import.py
+++ Edge_Parrot/test_import.py
@@ -2,4 +2,17 @@
 import olympe
 import os
 import time
-#from olympe.messages.ardrone3.Piloting import Takeoff, Landing
+from olympe.messages.ardrone3.PilotingState import PositionChanged
+
+# Connect to the drone
+drone = olympe.Drone("192.168.53.1")
+drone.connect()
+
+# Print the latitude and longitude of the drone
+print("Latitude:", drone.get_state(PositionChanged)["latitude"])
+print("Longitude:", drone.get_state(PositionChanged)["longitude"])
+
+# Disconnect from the drone
+drone.disconnect()
+
+
