1d4abe584d64967d407fe644f4e345883353cc58
hershelr3 <rinker.hershel@gmail.com>
2023-06-21 13:26:36 -0400

    edit

--- Edge_Parrot/test_import.py
+++ Edge_Parrot/test_import.py
@@ -1,30 +1,18 @@
 import olympe 
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy 
-from olympe.messages.ardrone3.PilotingState import (PositionChanged, AlertStateChanged, FlyingStateChanged, NavigateHomeStateChanged,)
+from olympe.messages.ardrone3.Piloting import NavigateHome
 import os
 import time
 # Connect to the drone
 #if it doesnt work then you have to physically takeoff and call client.calibrate(0) once in the air
 
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.53.1")
+drone = olympe.Drone(DRONE_IP)
+drone.connect()
 
-drone = olympe.Drone (DRONE_IP)
-#drone(setPilotingSource(source=\"SkyController\")).wait()
-class FlightListener(olympe.EventListener):
-    @olympe.listen_event(FlyingStateChanged() | AlertStateChanged() | 
-NavigateHomeStateChanged())
-    def onStateChanged(self, event, scheduler):
-        ...
-    @olympe.listen_event(PositionChanged())
-    def onPositionChanged(self, event, scheduler):
-        ...
-              
-with FlightListener(drone):
-    drone.connect()
-    drone(set_home_position(38.945901, -77.315607, 0)).wait()
-    drone.disconnect()
-
+drone(NavigateHome(lat=37.4219983, long=-122.084, altitude=10))
 
 print("Latitude:", drone.get_state(PositionChanged)["latitude"])
 print("Longitude:", drone.get_state(PositionChanged)["longitude"])
 print("Altitude:", drone.get_state(PositionChanged)["altitude"])
+
+drone.disconnect()
