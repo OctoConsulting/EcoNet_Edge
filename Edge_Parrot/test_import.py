087125ca69c83e5d950a8e522301e8bace7a228f
hershelr3 <rinker.hershel@gmail.com>
2023-06-21 11:06:50 -0400

    test

--- Edge_Parrot/test_import.py
+++ Edge_Parrot/test_import.py
@@ -3,19 +3,22 @@ import olympe
 import os
 import time
 # Connect to the drone
+#if it doesnt work then you have to physically takeoff and call client.calibrate(0) once in the air
 from olympe.messages.ardrone3.PilotingState import GpsLocationChanged
 from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+import olympe from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy from olympe.messages.ardrone3.PilotingState 
+import (PositionChanged, AlertStateChanged, FlyingStateChanged, NavigateHomeStateChanged,)
+
+
 
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.53.1")
 drone = olympe.Drone(DRONE_IP)
 drone.connect()
 
-def gps_callback(self, latitude, longitude, altitude):
-    print("Latitude:", latitude)
-    print("Longitude:", longitude)
-    print("Altitude:", altitude)
+drone(set_home_position(38.945901, -77.315607, 0)).wait()
 
-drone(GPSLocationChanged(_policy = 'wait', _no_expect = True)). \
-    each(gps_callback). \
-    wait()
+print("Latitude:", drone.get_state(PositionChanged)["latitude"])
+print("Longitude:", drone.get_state(PositionChanged)["longitude"])
+print("Altitude:", drone.get_state(PositionChanged)["altitude"])
 
+drone.disconnect()
