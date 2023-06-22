a40d8afcb293ba249668fff524a67f4fb64be2b4
Brissot <sidsu7@gmail.com>
2023-06-22 13:42:27 -0400

    janus

--- Edge_Video/setup.ps1
+++ Edge_Video/setup.ps1
@@ -1,2 +1,3 @@
+$ErrorActionPreference = "Stop" 
 docker build -t vid_mk1 .
 docker run -it --network="host" --mount type=bind,source="$env:USERPROFILE/Documents/GitHub/EcoNet_Edge",target=/mnt -p 8080:80 vid_mk1
