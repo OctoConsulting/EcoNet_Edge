06e91b3200c09899cf68096f9071dc9d79c3038f
Brissot <sidsu7@gmail.com>
2023-06-22 17:00:20 -0400

    final commit of the day

--- Edge_Video/reencode.sh
+++ Edge_Video/reencode.sh
@@ -1,1 +1,1 @@
-ffmpeg -i "rtsp://192.168.53.1/live" -hls_time 3 -hls_wrap 1 "/usr/share/nginx/html/streaming.m3u8"
+ffmpeg -i "rtsp://192.168.53.1/live" -hls_time 3 -hls_wrap 10 "/usr/share/nginx/html/streaming.m3u8"
