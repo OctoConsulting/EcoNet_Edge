e4ac3ba64fb9a456da0b53ff8abda9ab16bbdbf7
Chris Hung <68969347+InverseRex56@users.noreply.github.com>
2023-06-22 15:57:31 -0400

    update testing and compose

--- TotoTesting/Testing.py
+++ TotoTesting/Testing.py
@@ -1,18 +1,13 @@
 import simple_websocket
 
 def main():
-    ws = simple_websocket.Client('ws://{host_ip}:8001/toto/192.168.53.1')
+    ws = simple_websocket.Client('ws://localhost:8001/toto/192.168.53.1')
     try:
         while True:
             data = ws.receive()
             print(f'< {data}')
     except (KeyboardInterrupt, EOFError, simple_websocket.ConnectionClosed):
         ws.close()
-
-if __name__ == '__main__':
-    import os
-    host_ip = os.popen('ipconfig').read().split('IPv4 Address. . . . . . . . . . . : ')[1].split('\n')[0]
-    main()
-
+        
 if __name__ == '__main__':
     main()
