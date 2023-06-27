7fa603faef3227945a40aeb8f5c8c098187ed223
Saketh Rajesh <sakethraj122@gmail.com>
2023-06-27 15:52:32 -0400

    drone selector fixed

--- Main/deploy.py
+++ Main/deploy.py
@@ -1,13 +1,18 @@
 import os
 import sys
 import json 
+import requests
 
 def main():
     data = sys.argv[1]
     location = json.loads(data)
 
+    print(str(location), flush=True)
+
+    url ='http://proto1:5000'
+
     # drone manager
-    
+    response = requests.post(f'http://{url}/api/coordinates', json=location)
     
 
 if __name__ == '__main__':
