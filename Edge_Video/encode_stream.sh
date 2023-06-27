03d54458f7644d8bfca6d1f384a6f596d004a16c
Brissot <sidsu7@gmail.com>
2023-06-27 15:53:29 -0400

    works

--- Edge_Video/encode_stream.sh
+++ Edge_Video/encode_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i "rtsp://192.168.53.1/live" -hls_time 3 -hls_wrap 10 "/usr/share/nginx/html/streaming.m3u8"
+ffmpeg -rw_timeout 5000000 -i "rtsp://192.168.53.1/live" -hls_time 3 -hls_wrap 10 "/usr/share/nginx/html/streaming.m3u8"
--- Edge_Video/encode_vid.sh
+++ Edge_Video/encode_vid.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i "rtsp://192.168.53.1/live" -hls_time 3 -hls_wrap 10 "/usr/share/nginx/html/streaming.m3u8"
-ffmpeg -re -stream_loop -1 -i video.mp4 -hls_time 3 -hls_wrap 10 -c copy streaming.m3u8
+ffmpeg -re -stream_loop -1 -i test_vid.mp4 -hls_time 3 -hls_wrap 10 -c copy /usr/share/nginx/html/streaming.m3u8
