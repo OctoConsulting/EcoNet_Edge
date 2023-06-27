3af4fba93bd53fbe542886129279a10b23310adf
Brissot <sidsu7@gmail.com>
2023-06-27 16:08:13 -0400

    will finish tmr

--- Edge_Video/container_startup.sh
+++ Edge_Video/container_startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #waitress-serve --host 0.0.0.0 --port=80 app:app &
-#bash ./encode_vid.sh
-bash ./encode_stream.sh
+bash ./encode_vid.sh
+#bash ./encode_stream.sh
 #nginx -g daemon off;
