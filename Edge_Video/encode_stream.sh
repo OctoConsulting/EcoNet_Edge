6bdbfae6744f90a311edf85dbb239e3145a641a5
Brissot <sidsu7@gmail.com>
2023-07-03 09:29:33 -0400

    minor changes so it works on the edge device

--- Edge_Video/encode_stream.sh
+++ Edge_Video/encode_stream.sh
@@ -7,5 +7,8 @@
 #ffmpeg -rw_timeout 5000000 -i "rtsp://192.168.53.1/live" \
 #  -hls_time 3 -hls_wrap 10 "/usr/share/nginx/html/streaming.m3u8" #oldpath
 
-ffmpeg -rw_timeout 5000000 -i "rtsp://192.168.53.1/live" \
+#ffmpeg -rw_timeout 5000000 -i "rtsp://192.168.53.1/live" \
+#  -hls_time 3 -hls_wrap 10 "/srv/feed1/streaming.m3u8"
+
+ffmpeg -i "rtsp://192.168.53.1/live" \
   -hls_time 3 -hls_wrap 10 "/srv/feed1/streaming.m3u8"
